2e828cc3e68bed5e69bc80d2b77b5d7168ce82b9
yossuzuk <yossuzuk@nsc.nagoya-cu.ac.jp>
2026-06-26 15:57:55 +0900

    Delete src/sbsstr.h

--- src/sbsstr.h
+++
@@ -1,8 +0,0 @@
-#define MAXENT 5500
-#define MAXBUF 3000
-#define MAXSEQ 1710
-#define MAXNAME 20
-#define MAXDIG 20
-#define MAXPAIR 40000
-double mu[6][6];
-void funccod(char *, char *, char *, double *);
